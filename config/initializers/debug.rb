bfb926c8ec023b0089236a75f487786b1ebfd579
NGrande-SAI <ngrande@slower.ai>
2025-04-02 15:35:15 -0400

    Fix debugger config to only run on rails server (so other rails commands can be run without debugger attempting to bind to in-use port)

--- config/initializers/debug.rb
+++ config/initializers/debug.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? && defined?(::Rails::Server)
   require 'debug/session'
   Rails.logger.info "Starting debug session"
   DEBUGGER__.open(port: "12345", host: "0.0.0.0")
