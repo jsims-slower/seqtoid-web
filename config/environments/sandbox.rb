48962eea475e46e183e62a14f5c218a72cbb8c81
NGrande-SAI <ngrande@slower.ai>
2025-04-11 13:35:35 -0400

    Add new UCSF domain to host and cors config

--- config/environments/sandbox.rb
+++ config/environments/sandbox.rb
@@ -72,12 +72,12 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "idseq-#{Rails.env}"
 
-  config.action_mailer.default_url_options = { host: "#{Rails.env}.czid.org" }
+  config.action_mailer.default_url_options = { host: "#{Rails.env}.ucsf.czid.org" }
 
-  config.hosts << "sandbox.czid.org"
+  config.hosts << "sandbox.ucsf.czid.org"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # We configure IDseq to use cloudfront CDN when available.
-  config.asset_host = ENV["CZID_CLOUDFRONT_ENDPOINT"] || "#{Rails.env}.czid.org"
+  config.asset_host = ENV["CZID_CLOUDFRONT_ENDPOINT"] || "#{Rails.env}.ucsf.czid.org"
   # Custom config for idseq to enable CORS headers by environment. See rack_cors.rb.
   config.allowed_cors_origins = [
     "https://#{Rails.env}.idseq.net",
@@ -86,10 +86,14 @@ Rails.application.configure do
     "https://#{Rails.env}.czid.org",
     "https://www.#{Rails.env}.czid.org",
     "https://assets.#{Rails.env}.czid.org",
+    "https://#{Rails.env}.ucsf.czid.org",
+    "https://www.#{Rails.env}.ucsf.czid.org",
+    "https://assets.#{Rails.env}.ucsf.czid.org"
   ]
 
   config.middleware.use Rack::HostRedirect, "www.#{Rails.env}.idseq.net" => "#{Rails.env}.idseq.net"
   config.middleware.use Rack::HostRedirect, "www.#{Rails.env}.czid.org" => "#{Rails.env}.czid.org"
+  config.middleware.use Rack::HostRedirect, "www.#{Rails.env}.ucsf.czid.org" => "#{Rails.env}.ucsf.czid.org"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
